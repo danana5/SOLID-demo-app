1968fdd0102769a0776e52ddedc2428d258f89a2
Daniel Grace <daniel.grace44@hotmail.com>
2023-03-01 14:12:15 +0000

    added editing of profile locally

--- healthcare-app/src/pages/profile.js
+++ healthcare-app/src/pages/profile.js
@@ -1,9 +1,12 @@
 import HomeToolbar from "@/components/toolbar";
 import { useSession, CombinedDataProvider, Text } from "@inrupt/solid-ui-react";
-import { Typography, Container, Card, TextField, Button, CardActions } from "@mui/material";
+import { Typography, Container, Card, TextField, Button, CardActions, CardContent, CardHeader, Avatar } from "@mui/material";
 import { getProfile, getOrCreateContainer, getProfileData } from "@/utils/pods"
 import { useEffect, useState } from "react";
 import { getSolidDataset, getThing, getUrlAll, getStringNoLocale } from "@inrupt/solid-client";
+import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Cancel';
 import { STORAGE_PREDICATE, PROFILE } from "@/utils/predicates";
 
 
@@ -13,7 +16,10 @@ export default function ProfilePage() {
     const [container, setContainer] = useState()
     const [edit, setEdit] = useState(false)
     const [profile, setProfile] = useState({})
-    const [pods, setPods] = useState();
+    const [pods, setPods] = useState()
+    const [temp, setTemp] = useState()
+    let profilePage
+
 
 
     useEffect(() => {
@@ -29,8 +35,6 @@ export default function ProfilePage() {
             res.telephone = await getStringNoLocale(profileData, PROFILE.TELEPHONE)
             res.address = await getStringNoLocale(profileData, PROFILE.ADDRESS)
             setProfile(res)
-
-            console.log(profile)
         }
 
         if (!session.info.isLoggedIn) return;
@@ -50,23 +54,74 @@ export default function ProfilePage() {
         })();
     }, [session.info.isLoggedIn]);
 
-    // useEffect(() => {
-
-    // }, [edit])
-
     function handleEdit() {
-        console.log("Edit")
+        setTemp(profile)
         setEdit(!edit)
+    }
+
+    function getIntials() {
+        if (profile.givenName && profile.familyName) {
+            let initials = Array.from(profile.givenName)[0] + Array.from(profile.familyName)[0];
+            return initials
+        }
+    }
+
+    function handleSave() {
+        setProfile(temp)
+        setEdit(false)
+    }
+
+    function handleChange(evt) {
+        const value = evt.target.value;
+
+        setTemp({
+            ...temp,
+            [evt.target.name]: value
+        });
+    }
+
+    if (!edit) {
+        profilePage = (
+            <div>
+                <CardHeader title="Your Profile"></CardHeader>
+                <CardContent>
+                    <Avatar variant="rounded" sx={{ width: 80, height: 80, position: "center", mb: 3 }} >{getIntials()}</Avatar>
+                    <Typography>Name: {profile.givenName + ' ' + profile.familyName}</Typography>
+                    <Typography>Address: {profile.address}</Typography>
+                    <Typography>Contact Number: {profile.telephone}</Typography>
+                    <Typography>Email Address: {profile.email}</Typography>
+                    <Typography>You've Been a Member Since: {new Date(profile.dateCreated).toDateString()}</Typography>
+                </CardContent>
+                <CardActions><Button onClick={handleEdit} startIcon={<EditIcon />} >Edit</Button></CardActions>
+            </div>
+
+        )
+    }
+    else {
+        profilePage = (
+            <div>
+                <CardHeader title="Edit Your Profile"></CardHeader>
+                <CardContent>
+                    <Avatar variant="rounded" sx={{ width: 80, height: 80, position: "center", mb: 3 }} >{getIntials()}</Avatar>
+                    <div><TextField value={temp.givenName} onChange={handleChange} name="givenName" sx={{ mb: 1 }} label="First Name"></TextField></div>
+                    <div><TextField value={temp.familyName} onChange={handleChange} name="familyName" sx={{ mb: 1 }} label="Surname"></TextField></div>
+                    <div><TextField value={temp.address} onChange={handleChange} name="address" sx={{ mb: 1 }} label="Address"></TextField></div>
+                    <div><TextField value={temp.telephone} onChange={handleChange} name="telephone" sx={{ mb: 1 }} label="Telephone"></TextField></div>
+                    <div><TextField value={temp.email} onChange={handleChange} name="email" sx={{ mb: 1 }} label="Email"></TextField></div>
+                </CardContent >
+                <CardActions>
+                    <Button onClick={handleEdit} startIcon={<CancelIcon />} color="secondary">Cancel</Button>
+                    <Button onClick={handleSave} startIcon={<SaveIcon />} color="success">Save</Button>
+                </CardActions>
+            </div >
+        )
     }
 
     return (
         <div>
             <HomeToolbar></HomeToolbar>
             <Container>
-                <Card>
-                    <TextField variant="outlined" value={profile.dateCreated} label="Date Created" inputProps={{ readOnly: !edit }}></TextField>
-                    <CardActions><Button onClick={handleEdit}>Toggle Edit</Button></CardActions>
-                </Card>
+                <Card sx={{ mt: 5 }} variant="outlined">{profilePage}</Card>
             </Container>
         </div >
     )
